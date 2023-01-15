a3eac82133a189c8188e7e952d6960c5d26028b2
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-15 19:34:54 +0700

    Update type_parameter_test.go

--- type_parameter_test.go
+++ type_parameter_test.go
@@ -1,5 +1,22 @@
 package gogenerics
 
-func Length[T](){
-	
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Length[T any](param T) T {
+	fmt.Println(param)
+	return param
+}
+
+func TestSampleLength(t *testing.T) {
+	var res string = Length("OK")
+	assert.Equal(t, "OK", res)
+
+	var rate = Length(100)
+	assert.Equal(t, 100, rate)
+
 }
