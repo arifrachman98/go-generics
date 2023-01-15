de84fc582247a7fa66322f414ff7cf0b318e00a4
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-15 21:07:13 +0700

    Update type_set_test.go

--- type_set_test.go
+++ type_set_test.go
@@ -6,8 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type Range int
+
 type Number interface {
-	int | int16 | int32 | int64 |
+	~int | int16 | int32 | int64 |
 		float32 | float64
 }
 
@@ -23,4 +25,5 @@ func TestMin(t *testing.T) {
 	assert.Equal(t, 100, Min[int](100, 200))
 	assert.Equal(t, int64(100), Min[int64](int64(100), int64(200)))
 	assert.Equal(t, float64(0.33), Min[float64](0.33, 0.78500))
+	assert.Equal(t, Range(1000), Min[Range](Range(1000), Range(50000)))
 }
