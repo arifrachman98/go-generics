3d509f8a27ff4b6a7014402c77fe1bcf461526b1
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-15 21:03:36 +0700

    Add new file type_set_test.go

---
+++ type_set_test.go
@@ -0,0 +1,26 @@
+package gogenerics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Number interface {
+	int | int16 | int32 | int64 |
+		float32 | float64
+}
+
+func Min[T Number](first, second T) T {
+	if first < second {
+		return first
+	} else {
+		return second
+	}
+}
+
+func TestMin(t *testing.T) {
+	assert.Equal(t, 100, Min[int](100, 200))
+	assert.Equal(t, int64(100), Min[int64](int64(100), int64(200)))
+	assert.Equal(t, float64(0.33), Min[float64](0.33, 0.78500))
+}
