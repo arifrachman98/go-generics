10f8f668b844e5035d8664c805edfcaef9b1647d
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-16 22:43:59 +0700

    Update generic_inline_test.go

--- generic_inline_test.go
+++ generic_inline_test.go
@@ -2,8 +2,20 @@ package gogenerics
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestFindMin(t *testing.T) {
+func FindMin[T interface{ int | int64 | float64 }](first T, second T) T {
+	if first < second {
+		return first
+	} else {
+		return second
+	}
+}
 
+func TestFindMin(t *testing.T) {
+	assert.Equal(t, 100, FindMin(100, 200))
+	assert.Equal(t, int64(100), FindMin(int64(100), int64(200)))
+	assert.Equal(t, 100.0, FindMin(100.0, 200.0))
 }
