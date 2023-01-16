43a0576ff6161a95f308a1c09c0eb647bd50fd39
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-16 22:31:17 +0700

    Update generic_interface_test.go

---
+++ generic_interface_test.go
@@ -0,0 +1,23 @@
+package gogenerics
+
+type GetterSetter[T any] interface {
+	GetValue() T
+	SetValue(value T)
+}
+
+func ChangeValue[T any](param GetterSetter[T], value T) T {
+	param.SetValue(value)
+	return param.GetValue()
+}
+
+type MyData[T any] struct {
+	Value T
+}
+
+func (d *MyData[T]) GetValue() T {
+	return d.Value
+}
+
+func (d *MyData[T]) SetValue(value T) {
+	d.Value = value
+}
