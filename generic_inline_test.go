3834a8fe6a011d7274a89760a434aea5fd7527ba
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-16 22:48:18 +0700

    Update generic_inline_test.go

--- generic_inline_test.go
+++ generic_inline_test.go
@@ -14,8 +14,24 @@ func FindMin[T interface{ int | int64 | float64 }](first T, second T) T {
 	}
 }
 
+func GetFirst[T []E, E any](data T) E {
+	first := data[0]
+	return first
+}
+
 func TestFindMin(t *testing.T) {
 	assert.Equal(t, 100, FindMin(100, 200))
 	assert.Equal(t, int64(100), FindMin(int64(100), int64(200)))
 	assert.Equal(t, 100.0, FindMin(100.0, 200.0))
+}
+
+func TestGetFirst(t *testing.T) {
+	names := []string{
+		"Arif",
+		"Jaka",
+		"Jamal",
+	}
+
+	first := GetFirst(names)
+	assert.Equal(t, "Arif", first)
 }
