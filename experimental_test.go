8155ffd87f4539bf962e9825f75775d8acbb8e75
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-16 22:57:08 +0700

    Update experimental_test.go

--- experimental_test.go
+++ experimental_test.go
@@ -1,1 +1,21 @@
 package gogenerics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/exp/constraints"
+)
+
+func ExperimentalMin[T constraints.Ordered](first T, last T) T {
+	if first < last {
+		return first
+	} else {
+		return last
+	}
+}
+
+func TestExperimentalMin(t *testing.T) {
+	assert.Equal(t, 100, ExperimentalMin(100, 200))
+	assert.Equal(t, 100.0, ExperimentalMin(100.0, 200.0))
+}
